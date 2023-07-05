59f09393adb3ea7d6d4eca4a1ed2b69a7cc29f33
Yannick Yomie <yynzeuhang@gmail.com>
2023-07-05 01:41:02 +0100

    up

--- hmm_acoustic.py
+++ hmm_acoustic.py
@@ -22,7 +22,7 @@ SAMPLE_RATE = 22050
 MONO = True
 
 class HMMTrainer(object):
-    def __init__(self, model_name='GMMHMM', n_components=4, n_mix = 2, cov_type='diag', n_iter=1000):
+    def __init__(self, model_name='GMMHMM', n_components=5, n_mix = 6, cov_type='diag', n_iter=1000):
         self.model_name = model_name
         self.n_components = n_components
         self.cov_type = cov_type
@@ -106,7 +106,7 @@ if __name__ == "__main__":
             filepath = os.path.join(subfolder, filename)
                        
             mfcc_features = process(filepath)
-            mfcc_features = mfcc_features.reshape(len(mfcc_features),-1)
+            mfcc_features = mfcc_features.reshape((len(mfcc_features)*mfcc_features.shape[1]),1)
             length.append(len(mfcc_features))
             
             if len(X)== 0:
